6451c9c6398773f941b579c8c6e7b5a47ab5ee34
JDDo12 <jknnmooo13@gmail.com>
2026-08-07 17:47:05 +0900

    0807 수업내용

--- ex_0807/src/ex1_statement/Ex2_for.java
+++ ex_0807/src/ex1_statement/Ex2_for.java
@@ -2,21 +2,21 @@ package ex1_statement;
 
 public class Ex2_for {
     public static void main(String[] args) {
-        
-        //1부터 ~ 10까지의 정수들 중에서 홀수의 합만 계산하여 출력
-        //---------------------
-        //결과 : 25
+
+        // 1부터 ~ 10까지의 정수들 중에서 홀수의 합만 계산하여 출력
+        // ---------------------
+        // 결과 : 25
 
         int sum = 0;
-        for( int i = 1; i <= 10; i++ ){
+        for (int i = 1; i <= 10; i++) {
 
-            if( i % 2 == 1 ){
+            if (i % 2 == 1) {
                 sum += i;
-            }//if
+            } // if
 
-        }//for
+        } // for
 
         System.out.println("결과 : " + sum);
 
-    }//main
-}//class
+    }// main
+}// class
