2b5cb75021f84ed8c92b262ab2337855cd3e9324
JDDo12 <jknnmooo13@gmail.com>
2026-08-10 08:41:19 +0900

    0807 수업내용

--- ex_0807/src/ex_work/Ex1_work.java
+++ ex_0807/src/ex_work/Ex1_work.java
@@ -27,7 +27,15 @@ public class Ex1_work {
         System.out.print("연도 : ");
         int year = sc.nextInt();
 
-        System.out.println("윤년입니다");
+        if( year % 400 == 0 ){
+            System.out.println("윤년입니다");
+        } else if ( year % 100 == 0 ){
+            System.out.println("평년입니다");
+        } else if( year % 4 == 0 ){
+            System.out.println("윤년입니다");
+        } else {
+            System.out.println("평년입니다");
+        }
 
         
 
--- ex_0807/src/ex_work/Ex2_work.java
+++ ex_0807/src/ex_work/Ex2_work.java
@@ -1,7 +1,10 @@
 package ex_work;
 
+import java.util.Scanner;
+
 public class Ex2_work {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         //키보드에서 입력받은 2 ~ 9사이의 값에 해당하는
         //구구단을 출력
@@ -15,6 +18,13 @@ public class Ex2_work {
         // 5 * 2 = 10
         //.....
         //5 * 9 = 45
+
+        System.out.println("2 ~ 9 사이의 값을 입력하세요");
+        int dan = sc.nextInt();
+
+        for( int i = 1; i <= 9; i++){
+            System.out.println(dan + "*" + i + "=" +(dan * i));
+        }
 
     }//main
 
