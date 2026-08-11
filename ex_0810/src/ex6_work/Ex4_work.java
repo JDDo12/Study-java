1bb4a7fead57ffb1a7e7080a453144599bc23c7a
JDDo12 <jknnmooo13@gmail.com>
2026-08-11 17:00:27 +0900

    0810 수업내용

--- ex_0810/src/ex6_work/Ex4_work.java
+++ ex_0810/src/ex6_work/Ex4_work.java
@@ -1,19 +1,42 @@
 package ex6_work;
 
+import java.util.Scanner;
+
 public class Ex4_work {
     public static void main(String[] args) {
-        
-        //키보드에서 입력받은 두 개의 수의 최대공약수 출력
-        //-----------------------------------------
-        //수1 : 10
-        //수2 : 4
-        //최대공약수 : 2
-        //-----------------------------------------
-        //수1 : 18
-        //수2 : 12
-        //최대공약수 : 6
 
-        
-        
-    }//main
+        // 키보드에서 입력받은 두 개의 수의 최대공약수 출력
+        // -----------------------------------------
+        // 수1 : 10
+        // 수2 : 4
+        // 최대공약수 : 2
+        // -----------------------------------------
+        // 수1 : 18
+        // 수2 : 12
+        // 최대공약수 : 6
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("수1 : ");
+        int su1 = sc.nextInt();
+        System.out.print("수2 : ");
+        int su2 = sc.nextInt();
+
+        for (int i = 1; i <= su1; i++) {
+
+            for (int j = 1; j <= su2; j++) {
+
+                if (i % su1 == 0);
+                    
+
+                if (j % su2 == 0);
+                    
+
+            } // in
+
+        } // out
+
+        System.out.println("최대공약수 : " + su1 % su2);
+
+    }// main
 }
