dce4c6611325c9e6be696c49d0005effbd47eaa0
JDDo12 <jknnmooo13@gmail.com>
2026-08-14 12:42:51 +0900

    0814 수업내용

---
+++ ex_0814/src/ex_work/Ex03_work.java
@@ -0,0 +1,44 @@
+package ex_work;
+
+import java.util.Scanner;
+
+public class Ex03_work {
+    public static void main(String[] args) {
+
+        // 수1 : 10
+        // 수2 : 20
+        // 연상자 : +
+        // 10 + 20 = 30
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("수1 : ");
+        int su1 = sc.nextInt();
+        
+        System.out.print("수2 : ");
+        int su2 = sc.nextInt();
+        
+        System.out.print("연산자 : ");
+        String str = sc.next();
+
+        switch (str) {
+
+            case "+":
+                System.out.println(su1 + "+" + su2 + "=" + (su1 + su2));
+                break;
+            case "-":
+                System.out.println(su1 + "-" + su2 + "=" + (su1 - su2));
+                break;
+            case "*":
+                System.out.println(su1 + "*" + su2 + "=" + (su1 * su2));
+                break;
+            case "/":
+                System.out.println(su1 + "/" + su2 + "=" + ((float) su1 / su2));
+                break;
+            default:
+                System.out.println("연산자오류");
+                break;
+        }//switch
+
+    }// main
+}
