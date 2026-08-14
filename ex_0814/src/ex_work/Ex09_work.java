5cceecbb5e72fd37fd1356d334b4dcc321025337
JDDo12 <jknnmooo13@gmail.com>
2026-08-14 16:37:03 +0900

    0814 수업내용

---
+++ ex_0814/src/ex_work/Ex09_work.java
@@ -0,0 +1,26 @@
+package ex_work;
+
+import java.util.Random;
+
+public class Ex09_work {
+    public static void main(String[] args) {
+        /*
+        세자리 수를 입력하세요(예:123) - 123
+        1Strike, 1Ball
+        --------------------------
+        세자리 수를 입력하세요(예:123) - 567
+        OUT!!
+        --------------------------
+        세자리 수를 입력하세요(예:123) - 214
+        0Strike, 3Ball
+        --------------------------
+        세자리 수를 입력하세요(예:123) - 142
+        4회 정답 !! - 142
+        */
+
+        int base1 = new Random().nextInt(9) + 1;
+        int base2 = new Random().nextInt(9) + 1;
+        int base3 = new Random().nextInt(9) + 1;
+        
+    }// main
+}
