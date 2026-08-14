a5445b0cb977cb3e5e591e87ef757c60e085fe95
JDDo12 <jknnmooo13@gmail.com>
2026-08-14 17:45:05 +0900

    0814 수업내용

--- ex_0814/src/ex_work/Ex09_work.java
+++ ex_0814/src/ex_work/Ex09_work.java
@@ -1,10 +1,11 @@
 package ex_work;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ex09_work {
     public static void main(String[] args) {
-        /*
+        /*중복되지 않은 3자리 수 
         세자리 수를 입력하세요(예:123) - 123
         1Strike, 1Ball
         --------------------------
@@ -17,10 +18,32 @@ public class Ex09_work {
         세자리 수를 입력하세요(예:123) - 142
         4회 정답 !! - 142
         */
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        int i = 0;
+        int j = 0;
 
-        int base1 = new Random().nextInt(9) + 1;
-        int base2 = new Random().nextInt(9) + 1;
-        int base3 = new Random().nextInt(9) + 1;
+        int[] base = new int[3];
+
+        
+        out : for( i = 0; i < base.length;){
+
+            base[i] = new Random().nextInt(9) + 1;
+
+            for( j = 0; j < i; j++ ){
+
+                if( base[i] == base[j] ){
+                    continue out;
+                }//if
+  
+            }//in
+            
+            System.out.print(base[i] + "");
+            i++;
+
+        }//out
+        
+
         
     }// main
 }
