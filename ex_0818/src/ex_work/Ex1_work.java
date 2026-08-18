a1fb47082fdb4d0cf09a4292d970bc6b61a12c05
JDDo12 <jknnmooo13@gmail.com>
2026-08-18 17:51:22 +0900

    0818 수업내용

---
+++ ex_0818/src/ex_work/Ex1_work.java
@@ -0,0 +1,15 @@
+package ex_work;
+
+public class Ex1_work {
+    public static void main(String[] args) {
+        
+        //키보드에서 입력받은 값이 회문인지 판단
+        //--------------------------------
+        //입력 : 스윙스
+        //스윙스은(는) 회문입니다
+
+        //입력 : 스윙칩
+        //스윙칩은(는) 회문이 아닙니다
+
+    }//main
+}
