0a4d5a064ad805c3aeb327ff1b446882755acef8
JDDo12 <jknnmooo13@gmail.com>
2026-08-21 16:10:07 +0900

    0820 수업내용

--- ex_0820/src/ex04_work/Vending.java
+++ ex_0820/src/ex04_work/Vending.java
@@ -1,69 +1,77 @@
 package ex04_work;
 
 public class Vending {
-    
+
     private Can[] cans = new Can[5];
     private int money;
 
-    //자판기에서 관리할 음료수를 준비
-    public void init(){
+    // 자판기에서 관리할 음료수를 준비
 
-        for( int i = 0; i < cans.length; i++ ){
-            cans[i] = new Can();
+    public Vending(){
+        cans[0] = new Can("환타", 1000);
+        cans[1] = new Can("스프라이트", 1200);
+        cans[2] = new Can("코카콜라", 1300);
+        cans[3] = new Can("펩시", 1400);
+        cans[4] = new Can("몬스터", 1500);
+    }
 
-        }//for
+    // public void init(){
 
-        cans[0].setName("환타");
-        cans[0].setPrice(1000);
-        
-        cans[1].setName("스프라이트");
-        cans[1].setPrice(1200);
-        
-        cans[2].setName("코카콜라");
-        cans[2].setPrice(1300);
-        
-        cans[3].setName("펩시");
-        cans[3].setPrice(1400);
-        
-        cans[4].setName("몬스터");
-        cans[4].setPrice(1500);
+    // for( int i = 0; i < cans.length; i++ ){
+    // cans[i] = new Can();
 
-    }//init()
+    // }//for
 
-    //사용자의 돈을 받는다
-    //이 돈으로 먹을 수 있는 음료 목록만 보여주기
-    public void showDrink( int money ){
+    // cans[0].setName("환타");
+    // cans[0].setPrice(1000);
 
-        for( int i = 0; i < cans.length; i++ ){
+    // cans[1].setName("스프라이트");
+    // cans[1].setPrice(1200);
 
-            if( cans[i].getPrice() <= money ){
+    // cans[2].setName("코카콜라");
+    // cans[2].setPrice(1300);
+
+    // cans[3].setName("펩시");
+    // cans[3].setPrice(1400);
+
+    // cans[4].setName("몬스터");
+    // cans[4].setPrice(1500);
+
+    // }//init()
+
+    // 사용자의 돈을 받는다
+    // 이 돈으로 먹을 수 있는 음료 목록만 보여주기
+    public void showDrink(int money) {
+
+        for (int i = 0; i < cans.length; i++) {
+
+            if (cans[i].getPrice() <= money) {
                 System.out.printf("%s - %d원\n", cans[i].getName(), cans[i].getPrice());
             }
 
-        }//for
+        } // for
 
         this.money = money;
 
         System.out.print(">> ");
 
-    }//showDrink()
+    }// showDrink()
 
-    //사용자가 음료수를 선택하면
-    //해당 음료수를 제공하고 잔돈을 돌려준다
-    public void dispense( String name ){
+    // 사용자가 음료수를 선택하면
+    // 해당 음료수를 제공하고 잔돈을 돌려준다
+    public void dispense(String name) {
 
-        for( int i = 0; i < cans.length; i++ ){
+        for (int i = 0; i < cans.length; i++) {
 
-            if( cans[i].getName().equals(name) ){
+            if (cans[i].getName().equals(name)) {
                 System.out.println(name + "을(를) 선택함");
-                money -= cans[i].getPrice();//잔돈계산
+                money -= cans[i].getPrice();// 잔돈계산
 
                 System.out.println("잔액 : " + money);
             }
 
-        }//for
+        } // for
 
-    }//duspense()
-
+    }// duspense()
 
 }
